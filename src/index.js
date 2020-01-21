0828d50ffe91aa138d842489764f5495cc7a460f
morganl92 <morganlinbox@gmail.com>
2020-01-20 17:59:25 -0800

    [prog] removes webfontloader

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import WebFont from 'webfontloader'
 import App from './App';
-
-WebFont.load({
-  google: {
-    families: ['Lato:300,400,700', 'sans-serif']
-  }
-});
 
 ReactDOM.render(<App />, document.getElementById('app'));
