0f66acdd0247b05c59dd3256fe350ae8b3f45d25
morganl92 <morganlinbox@gmail.com>
2020-01-20 21:23:59 -0800

    [feat] completes stacks component

--- src/components/stacks/stacks.component.jsx
+++ src/components/stacks/stacks.component.jsx
@@ -1,113 +1,166 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const frontend = [
-  'Javascript (ES5 and ES6)',
-  'HTML5',
-  'CSS3',
-  'React',
-  'React Native'
-]
+import './stacks.styles.scss';
 
-const backend = [
-  'Node.js',
-  'Express',
-  'MongoDB',
-  'Mongoose',
-  'PostgreSQL',
-  'MySQL',
-  'Sequelize',
-]
+const Stack = ({ technology, stacks }) => {
+  return (
+    <>
+      <h3>
+        {
+          technology === 'Deployment' ? 'Deployment / Testing'
+          : technology === 'Skills' ? 'Developer Skills'
+          : technology
+        }
+      </h3>
+      <div className='technology'>
+        {stacks.map((stack, i) => (
+          <div className='item'>
+            <img className='icon' src={stack.image} /> <br />
+            <span key={i}>{stack.name}</span>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+};
 
-const deployment = [
-  'AWS S3 & EC2',
-  'NGINX',
-  'Heroku',
-  'Artillery',
-  'Loader',
-  'New Relic'
-]
+class Stacks extends Component {
+  constructor(props) {
+    super(props);
 
-const skills = [
-  'Git',
-  'yarn',
-  'npm',
-  'Webpack',
-  'Babel',
-  'Agile',
-  'Kanban'
-]
+    this.state = {
+      stacks: {
+        Frontend: [
+          {
+            name: 'Javascript (ES5 and ES6)',
+            image: 'https://cdn.svgporn.com/logos/javascript.svg'
+          },
+          {
+            name: 'HTML5',
+            image: 'https://cdn.svgporn.com/logos/html-5.svg'
+          },
+          {
+            name: 'CSS3',
+            image: 'https://cdn.svgporn.com/logos/css-3.svg'
+          },
+          {
+            name: 'React',
+            image: 'https://cdn.svgporn.com/logos/react.svg'
+          },
+          {
+            name: 'React Native',
+            image: 'https://cdn.svgporn.com/logos/react.svg'
+          } 
+        ],
+        Backend: [
+          {
+            name: 'Node.js',
+            image: 'https://cdn.svgporn.com/logos/nodejs.svg'
+          },
+          {
+            name: 'Express',
+            image: 'https://cdn.svgporn.com/logos/express.svg'
+          },
+          {
+            name: 'MongoDB',
+            image: 'https://cdn.svgporn.com/logos/mongodb.svg'
+          },
+          {
+            name: 'Mongoose',
+            image: 'https://mongoosejs.com/docs/images/mongoose5_62x30_transparent.png'
+          },
+          {
+            name: 'PostgreSQL',
+            image: 'https://cdn.svgporn.com/logos/postgresql.svg'
+          },
+          {
+            name: 'MySQL',
+            image: 'https://cdn.svgporn.com/logos/mysql.svg'
+          },
+          {
+            name: 'Sequelize',
+            image: 'https://cdn.svgporn.com/logos/sequelize.svg'
+          }
+        ],
+        Deployment: [
+          {
+            name: 'AWS EC2',
+            image: 'https://cdn.svgporn.com/logos/aws-ec2.svg'
+          },
+          {
+            name: 'AWS S3',
+            image: 'https://cdn.svgporn.com/logos/aws-s3.svg'
+          },
+          {
+            name: 'NGINX',
+            image: 'https://cdn.svgporn.com/logos/nginx.svg'
+          },
+          {
+            name: 'Heroku',
+            image: 'https://cdn.svgporn.com/logos/heroku-icon.svg'
+          },
+          {
+            name: 'Artillery',
+            image: 'https://camo.githubusercontent.com/b386570e1c4595293de68557663ca47ac522b55c/68747470733a2f2f617274696c6c6572792e696f2f696d672f666c61672e706e67'
+          },
+          {
+            name: 'loader',
+            image: 'https://cdn.svgporn.com/logos/loader.svg'
+          },
+          {
+            name: 'New Relic',
+            image: 'https://newrelic.com/assets/newrelic/source/SVG/NR_logo_Horizontal.svg'
+          }
+        ],
+        Skills: [
+          {
+            name: 'Git',
+            image: 'https://cdn.svgporn.com/logos/github-icon.svg'
+          },
+          {
+            name: 'yarn',
+            image: 'https://cdn.svgporn.com/logos/yarn.svg'
+          },
+          {
+            name: 'npm',
+            image: 'https://cdn.svgporn.com/logos/npm-2.svg'
+          },
+          {
+            name: 'Webpack',
+            image: 'https://cdn.svgporn.com/logos/webpack.svg'
+          },
+          {
+            name: 'Babel',
+            image: 'https://cdn.svgporn.com/logos/babel.svg'
+          },
+          {
+            name: 'Agile',
+            image: 'https://azumo.co/wp-content/uploads/2018/08/illustration-agile-methogology.svg'
+          },
+          {
+            name: 'Kanban',
+            image: 'https://cdn.svgporn.com/logos/trello.svg'
+          }
+        ]
+      }
+    }
+  }
 
-const Stacks = (props) => (
-  <section id='stacks' style={style}>
-    <p style={title}>
-      Technical Stacks
-    </p>
+  render() {
+    const { stacks } = this.state;
 
-    <p style={subtitle}>
-      Frontend
-    </p>
-    <div style={Object.assign({}, flexContainer, flexRow)}>
-      {frontend.map(((stack, i) => <p key={i} style={flexItem}>{[stack]}</p>))}
-    </div>
+    return (
+      <section id='stacks' className='stacks'>
+        <div className='container'>
+          <h1>
+            Technical Stacks
+          </h1>
 
-    <p style={subtitle}>
-      Backend
-    </p>
-    <div style={Object.assign({}, flexContainer, flexRow)}>
-      {backend.map(((stack, i) => <p key={i} style={flexItem}>{[stack]}</p>))}
-    </div>
-
-    <p style={subtitle}>
-      Deployment / Testing
-    </p>
-    <div style={Object.assign({}, flexContainer, flexRow)}>
-      {deployment.map(((stack, i) => <p key={i} style={flexItem}>{[stack]}</p>))}
-    </div>
-
-    <p style={subtitle}>
-      Developer Skills
-    </p>
-    <div style={Object.assign({}, flexContainer, flexRow)}>
-      {skills.map(((stack, i) => <p key={i} style={flexItem}>{[stack]}</p>))}
-    </div>
-  </section>
-)
-
-const style = {
-  // padding: '64px 512px 16px 512px',
-  // fontFamily: 'Avenir'
-  backgroundColor: '#EEEEEE'
+          {Object.entries(stacks).map(([technology, stacks], i) => <Stack key={i} technology={technology} stacks={stacks}/>)}
+        </div>
+      </section>
+    )
+  }
 }
-
-const title = {
-  textAlign: 'center',
-  fontWeight: 'bold',
-  fontSize: '24px',
-}
-
-const subtitle = {
-  fontSize: '16px',
-  fontWeight: 'bold',
-  textAlign: 'center'
-}
-
-const flexContainer = {
-  padding: '0',
-  margin: '0',
-  listStyle: 'none',
-  display: 'flex'
-}
-const flexRow = {
-  flexDirection: 'row',
-  justifyContent: 'center'
-}
-
-const flexItem = {
-  margin: '5px',
-  padding: '5px',
-}
-
-
-
 
 export default Stacks;
