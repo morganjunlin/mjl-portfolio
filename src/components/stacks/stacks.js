2eff0c7e424dc5ffce2ab42b42dbe47d034bd865
morganl92 <morganlinbox@gmail.com>
2020-01-21 13:36:37 -0800

    [style] separates stacks to independent file

---
+++ src/components/stacks/stacks.js
@@ -0,0 +1,140 @@
+module.exports = {
+  Frontend: [
+    {
+      id: 'js',
+      name: 'Javascript (ES5 and ES6)',
+      image: 'https://cdn.svgporn.com/logos/javascript.svg'
+    },
+    {
+      id: 'html5',
+      name: 'HTML5',
+      image: 'https://cdn.svgporn.com/logos/html-5.svg'
+    },
+    {
+      id: 'css3',
+      name: 'CSS3',
+      image: 'https://cdn.svgporn.com/logos/css-3.svg'
+    },
+    {
+      id: 'react',
+      name: 'React',
+      image: 'https://cdn.svgporn.com/logos/react.svg'
+    },
+    {
+      id: 'react native',
+      name: 'React Native',
+      image: 'https://cdn.svgporn.com/logos/react.svg'
+    } 
+  ],
+  Backend: [
+    {
+      id: 'node',
+      name: 'Node.js',
+      image: 'https://cdn.svgporn.com/logos/nodejs.svg'
+    },
+    {
+      id: 'express',
+      name: 'Express',
+      image: 'https://cdn.svgporn.com/logos/express.svg'
+    },
+    {
+      id: 'mongo',
+      name: 'MongoDB',
+      image: 'https://cdn.svgporn.com/logos/mongodb.svg'
+    },
+    {
+      id: 'mongoose',
+      name: 'Mongoose',
+      image: 'https://mongoosejs.com/docs/images/mongoose5_62x30_transparent.png'
+    },
+    {
+      id: 'psql',
+      name: 'PostgreSQL',
+      image: 'https://cdn.svgporn.com/logos/postgresql.svg'
+    },
+    {
+      id: 'mysql',
+      name: 'MySQL',
+      image: 'https://cdn.svgporn.com/logos/mysql.svg'
+    },
+    {
+      id: 'sequelize',
+      name: 'Sequelize',
+      image: 'https://cdn.svgporn.com/logos/sequelize.svg'
+    }
+  ],
+  Deployment: [
+    {
+      id: 'ec2',
+      name: 'AWS EC2',
+      image: 'https://cdn.svgporn.com/logos/aws-ec2.svg'
+    },
+    {
+      id: 's3',
+      name: 'AWS S3',
+      image: 'https://cdn.svgporn.com/logos/aws-s3.svg'
+    },
+    {
+      id: 'nginx',
+      name: 'NGINX',
+      image: 'https://cdn.svgporn.com/logos/nginx.svg'
+    },
+    {
+      id: 'heroku',
+      name: 'Heroku',
+      image: 'https://cdn.svgporn.com/logos/heroku-icon.svg'
+    },
+    {
+      id: 'artillery',
+      name: 'Artillery',
+      image: 'https://camo.githubusercontent.com/b386570e1c4595293de68557663ca47ac522b55c/68747470733a2f2f617274696c6c6572792e696f2f696d672f666c61672e706e67'
+    },
+    {
+      id: 'loader',
+      name: 'loader',
+      image: 'https://cdn.svgporn.com/logos/loader.svg'
+    },
+    {
+      id: 'newrelic',
+      name: 'New Relic',
+      image: 'https://newrelic.com/assets/newrelic/source/SVG/NR_logo_Horizontal.svg'
+    }
+  ],
+  Skills: [
+    {
+      id: 'git',
+      name: 'Git',
+      image: 'https://cdn.svgporn.com/logos/github-icon.svg'
+    },
+    {
+      id: 'yarn',
+      name: 'yarn',
+      image: 'https://cdn.svgporn.com/logos/yarn.svg'
+    },
+    {
+      id: 'npm',
+      name: 'npm',
+      image: 'https://cdn.svgporn.com/logos/npm-2.svg'
+    },
+    {
+      id: 'webpack',
+      name: 'Webpack',
+      image: 'https://cdn.svgporn.com/logos/webpack.svg'
+    },
+    {
+      id: 'babel',
+      name: 'Babel',
+      image: 'https://cdn.svgporn.com/logos/babel.svg'
+    },
+    {
+      id: 'agile',
+      name: 'Agile',
+      image: 'https://azumo.co/wp-content/uploads/2018/08/illustration-agile-methogology.svg'
+    },
+    {
+      id: 'kanban',
+      name: 'Kanban',
+      image: 'https://cdn.svgporn.com/logos/trello.svg'
+    }
+  ]
+}
