62b897f73759dffbeb41699a127c71ef4a61f668
morganl92 <morganlinbox@gmail.com>
2020-01-20 17:58:21 -0800

    [prog] imports css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 
+import './App.css';
+
 import Header from './components/header/header.component';
 import Navigation from './components/navigation/navigation.component';
 import About from './components/about/about.component';
-import Projects from './components/projects/projects.component';
 import Stacks from './components/stacks/stacks.component';
+import Projects from './components/projects/projects.component';
 import Contact from './components/contact/contact.component';
 import Footer from './components/footer/footer.component';
 
 
 export default function App() {
   return (
-    <div>
+    <>
       <Header />
       <Navigation />
       <About />
-      <Projects />
       <Stacks />
+      <Projects />
       <Contact />
       <Footer />
-    </div>
+    </>
   )
 };
