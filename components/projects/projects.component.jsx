0af31f00a0a1519e8c09d4a3355c0a37e24f4fb2
morganl92 <morganlinbox@gmail.com>
2020-02-23 17:43:03 -0800

    [feat] refactors to utilize modal

--- components/projects/projects.component.jsx
+++ components/projects/projects.component.jsx
@@ -1,72 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './projects.styles.scss';
 
-const Projects = ({ projectsRef }) => (
-  <section id='projects' className='projects' ref={projectsRef}>
-    <h1 className='title'>
-      APPLICATIONS
-    </h1>
+const Projects = ({ projectsRef, handleModal, productJSON }) => {
+  const [mask, setMask] = useState(false);
+  const [maskID, setMaskID] = useState();
 
-    <h3 className='title'>
-      Platonic.ly: Event Finder Social Media Application
-    </h3>
-    <div className='container'>
-      <div className='item expo'>
-        <iframe src='https://appetize.io/embed/xc1w6f1krd589zhp22a0mgftyw?device=nexus5&launchUrl=https%3A%2F%2Fexpo.io%2F%40morganjunlin%2Fplatonicly&pc=&screenOnly=false&autoplay=false&embed=true&xdocMsg=true&xDocMsg=true&debug=true&deviceColor=black&orientation=portrait&params=%7B%22EXKernelLaunchUrlDefaultsKey%22%3A%22https%3A%2F%2Fexpo.io%2F%40morganjunlin%2Fplatonicly%22%2C%22EXKernelDisableNuxDefaultsKey%22%3Atrue%7D' height='600px' frameBorder='0' scrolling='no'></iframe>
-      </div>
-      <div className='item'>
-        <ul>
-          <li>Provided users with multiple sign-in options, including Facebook, by utilizing OAuth 2.0 as the authentication protocol</li>
-          <li>Routed individual user profile pages to be viewed from event listings with React Navigation, as well as dynamically render events the user is attending</li>
-          <li>Designed relational database schema for complex SQL operations while adhering to RESTful API</li>
-        </ul>
-        <div className='btn-container'>
-          <input type='button' value='View on GitHub' onClick={() => window.location.href='https://github.com/morganjunlin/platonic.ly'}/>
-        </div>
-      </div>
-    </div>
+  const handleMask = (i) => {
+    setMask(prevState => !prevState)
+    setMaskID(i)
+  };
 
-    
+  return (
+    <section id='projects' className='projects' ref={projectsRef}>
+      <h1 className='title'>
+        APPLICATIONS
+      </h1>
 
-    <h3 className='title'>
-      Grubhub Legacy UX Clone System Optimization
-    </h3>
-    <div className='container'>
-      <div className='item'>
-        <img className='gif' src='https://mjl-portfolio.s3.us-east-2.amazonaws.com/grubhub.gif' />
-      </div>
-      <div className='item'>
-        <ul>
-          <li>Optimized database to handle queries containing over 10,000,000 records by indexing the database to the data’s lookup ID to allow for faster lookup</li>
-          <li>Increased throughput queries of 1000 rps to 2000 rps with a latency of 15 ms for 1 minute by horizontally scaling the service to 3 deployed EC2 instances for scalability</li>
-          <li>Load-balancing traffic using NGINX through continuous testing for stability of heavy incoming user traffic using load tester tools such as Artillery.io and Loader.io</li>
-        </ul>
-        <div className='btn-container'>
-          <input type='button' value='View on GitHub' onClick={() => window.location.href='https://github.com/morganjunlin/time-sponsored'}/>
-        </div>
-      </div>
-    </div>
+      <div className='container'>
+        {productJSON.map((product, i) => {
+          let { title, subtitle, stacks, css } = product
 
-    <h3 className='title'>
-      Nordstrom UX Clone
-    </h3>
-    <div className='container'>
-      <div className='item'>
-        <img className='gif' src='https://mjl-portfolio.s3.us-east-2.amazonaws.com/nordstrom.gif' />
+          return (
+            <div className='item' key={i}>
+              {mask && maskID === i ? 
+                <div className='mask' onMouseLeave={() => handleMask(i)}>
+                  <div className='mask-item'>
+                    <h1>{title}</h1>
+                    <h4>{stacks}</h4>
+                    <button onClick={() => {handleModal(i); handleMask(i)}}>
+                      View Project
+                    </button>
+                  </div>
+                </div> : null}
+              <div id={css} className='project' onMouseOver={() => handleMask(i)}>
+              </div>
+              <div className='description'>
+                <h2>{title}</h2>
+                <h4><em>{subtitle}</em></h4>
+              </div>
+            </div>
+          )})}
       </div>
-      <div className='item'>
-        <ul>
-          <li>Recreated functionality of the nav-items by storing individual department into categories and mathematically dividing these categories through CSS widths and floats</li>
-          <li>Incorporated a micro-service architecture to isolate maintainability of data and error handling</li>
-          <li>Recreated the search bar by conditionally rendering search results based on keywords drawn from self-generated data within MongoDB and speeding up search times by indexing user inputted search queries</li>
-        </ul>
-        <div className='btn-container'>
-          <input type='button' value='View on GitHub' onClick={() => window.location.href='https://github.com/tormund-and-big-lady/nav-bar-service'}/>
-        </div>
-      </div>
-    </div>
-  </section>
-)
+    </section>
+  )
+}
+
 
 export default Projects;
---
+++ components/projects/projects.js
@@ -0,0 +1,44 @@
+module.exports = [
+  {
+    title: 'Platonic.ly',
+    subtitle: 'Event Finder Social Media Application',
+    stacks: 'React Native | Express | PostgreSQL',
+    description: [
+      'Provided users with multiple sign-in options, including Facebook, by utilizing OAuth 2.0 as the authentication protocol',
+      'Routed individual user profile pages to be viewed from event listings with React Navigation, as well as dynamically render events the user is attending',
+      'Designed relational database schema for complex SQL operations while adhering to RESTful API'
+    ],
+    images: [
+      'https://mjl-portfolio.s3.us-east-2.amazonaws.com/mvp.png'
+    ],
+    css: 'mvp'
+  },
+  {
+    title: 'Grubhub Clone',
+    subtitle: 'Legacy UX Clone System Optimization',
+    stacks: 'Express | PostgreSQL | MongoDB | AWS EC2 | NGINX | Loader',
+    description: [
+      'Optimized database to handle queries containing over 10,000,000 records by indexing the database to the data’s lookup ID to allow for faster lookup',
+      'Increased throughput queries of 1000 rps to 2000 rps with a latency of 15 ms for 1 minute by horizontally scaling the service to 3 deployed EC2 instances for scalability',
+      'Load-balancing traffic using NGINX through continuous testing for stability of heavy incoming user traffic using load tester tools such as Artillery.io and Loader.io'
+    ],
+    images: [
+      'https://mjl-portfolio.s3.us-east-2.amazonaws.com/sdc.png'
+    ],
+    css: 'sdc'
+  },
+  {
+    title: 'Nordstrom Clone',
+    subtitle: 'Microservice Architecture UX Clone',
+    stacks: 'React | Express | MongoDB',
+    description: [
+      'Recreated functionality of the nav-items by storing individual department into categories and mathematically dividing these categories through CSS widths and floats',
+      'Incorporated a micro-service architecture to isolate maintainability of data and error handling',
+      'Recreated the search bar by conditionally rendering search results based on keywords drawn from self-generated data within MongoDB and speeding up search times by indexing user inputted search queries'
+    ],
+    images: [
+      'https://mjl-portfolio.s3.us-east-2.amazonaws.com/fec.png'
+    ],
+    css: 'fec'
+  }
+]
