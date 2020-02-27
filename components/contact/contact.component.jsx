7062e27573c0e32b44e67a16d26c169e691892a0
morganl92 <morganlinbox@gmail.com>
2020-02-26 16:56:07 -0800

    [feat] adds contact form

--- components/contact/contact.component.jsx
+++ components/contact/contact.component.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './contact.styles.scss';
+// import send from './send.php';
 
-const Contact = ({ contactRef }) => (
-  <section id='contact' className='contact' ref={contactRef}>
-    <h1>
-      CONTACT
-    </h1>
+const Contact = ({ contactRef }) => {
 
-    <div>
-      <img src='https://mjl-portfolio.s3.us-east-2.amazonaws.com/email-13.svg' />
-      <img src='https://mjl-portfolio.s3.us-east-2.amazonaws.com/linkedin-3.svg' onClick={() => window.location.href='https://www.linkedin.com/in/morganjunlin/'} />
-      <img src='https://mjl-portfolio.s3.us-east-2.amazonaws.com/github-1.svg' onClick={() => window.location.href='https://github.com/morganjunlin/'}/>
-    </div>
-  </section>
-)
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleNameChange = (e) => setName(e.target.value);
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handleMessageChange = (e) => setMessage(e.target.value);
+
+  return (
+    <section id='contact' className='contact' ref={contactRef}>
+      <h1>
+        CONTACT
+      </h1>
+
+      <form className='contact-form' method='POST' action='./send.php'>
+        <div><input type='text' id='name' name='name' placeholder='Name' onChange={handleNameChange} /></div>
+        <div><input type='text' id='email' name='email' placeholder='Email' onChange={handleEmailChange} /></div>
+        <div><textarea type='text' id='message' name='message' placeholder='Message' onChange={handleMessageChange} /></div>
+        <div><input type='submit' id='submit' value='Submit' /></div>
+      </form>
+    </section>
+  )
+}
 
 export default Contact;
