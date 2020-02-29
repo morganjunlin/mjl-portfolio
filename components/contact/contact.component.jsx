beff2e6f1ff8dff71462f0bba96bb2b141af98e3
morganl92 <morganlinbox@gmail.com>
2020-02-28 21:23:49 -0800

    [feat] adds working contact form

--- components/contact/contact.component.jsx
+++ components/contact/contact.component.jsx
@@ -8,10 +8,31 @@ const Contact = ({ contactRef }) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [status, setStatus] = useState('')
 
   const handleNameChange = (e) => setName(e.target.value);
   const handleEmailChange = (e) => setEmail(e.target.value);
   const handleMessageChange = (e) => setMessage(e.target.value);
+
+  const submitForm = (ev) => {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        setStatus('SUCCESS');
+        alert('Thank you for your message! I will respond back within 24-48 hours!');
+      } else {
+        setStatus('ERROR')
+      }
+    };
+    xhr.send(data);
+  }
 
   return (
     <section id='contact' className='contact' ref={contactRef}>
@@ -24,11 +45,17 @@ const Contact = ({ contactRef }) => {
         CONTACT
       </h1>
       
-      <form className='contact-form' method='POST' action='./send.php'>
-        <div><input type='text' id='name' name='name' placeholder='Name' onChange={handleNameChange} /></div>
-        <div><input type='text' id='email' name='email' placeholder='Email' onChange={handleEmailChange} /></div>
-        <div><textarea type='text' id='message' name='message' placeholder='Message' onChange={handleMessageChange} /></div>
+      <form 
+        className='contact-form' 
+        method='POST' 
+        action='https://formspree.io/xnqvzynq'
+        onSubmit={submitForm}
+      >
+        <div><input type='text' id='name' name='name' placeholder='Name' onChange={handleNameChange} required /></div>
+        <div><input type='email' id='email' name='email' placeholder='Email' onChange={handleEmailChange} required /></div>
+        <div><textarea type='text' id='message' name='message' placeholder='Message' onChange={handleMessageChange} required /></div>
         <div><input type='submit' id='submit' value='Submit' /></div>
+        {status === 'ERROR' && alert('Ooops! There was an error.')}
       </form>
     </section>
   )
